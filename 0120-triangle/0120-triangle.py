40abdd67a73d8b9f48db54eb229c08cb87c8484b
nikhil <b.nikhilkumar26@gmail.com>
2025-04-23 07:29:16 +0530

    Time: 1 ms (93.11%), Space: 13 MB (80.77%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        rows = len(triangle)
+        dp = triangle[-1][:]
+
+        for row in range(rows - 2, -1, -1):
+            for col in range(row + 1):
+                dp[col] = triangle[row][col] + min(dp[col], dp[col + 1])
+
+        return dp[0]
+        
