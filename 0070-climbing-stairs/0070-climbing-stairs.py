5511644a2f0be4a77c683f8175c14401f888ff7f
nikhil <b.nikhilkumar26@gmail.com>
2025-05-02 21:45:31 +0530

    Time: 0 ms (100%), Space: 12.5 MB (53.12%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        a,b = 1,2
+        for _ in range(n - 1):
+            a, b = b, a + b
+        return a 
+        
